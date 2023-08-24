7d774b201fa3e21db2d804df30d4f47a61b41feb
Edi Nugroho <edinugroho514@gmail.com>
2023-08-24 12:29:32 +0700

    Add prettier for class tailwindcss

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  tailwindConfig: "./tailwind.config.js",
+  bracketSameLine: true,
+  jsxBracketSameLine: true,
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: "ignore",
+  plugins: ["prettier-plugin-tailwindcss"],
+};
